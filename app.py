5920520c5cb83e2922fafedfd1f7288c39409f37
Mohit Khanwale <mohit.khanwale@gmail.com>
2020-04-29 00:33:54 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  7 23:31:09 2020
+@author: Mohit
+"""
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+from cv_parser import extract_info
+import pandas as pd
+import sqlite3
+
+UPLOAD_FOLDER = './Uploaded_Files'  # PATH TO STORE THE UPLOADED RESUMES
+DOWNLOAD_FOLDER = './Output_Files'  # PATH TO STORE THE OUTPUT EXCEL FILES
+ALLOWED_EXTENSIONS = {'pdf', 'docx'}
+app = Flask(__name__, static_folder='static')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+app.secret_key = 'super secret key'
+
+# METHOD TO CHECK IF THE FILE EXTENSION IS PDF OR DOCX ONLY
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# ROUTE TO ALLOW USER TO UPLOAD A FILE
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            saved_file = open(os.path.join(
+                app.config['UPLOAD_FOLDER'], filename), "rb")
+            bin_data = saved_file.read()
+            database(name=filename, data=bin_data)
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template("template.html")
+
+# ROUTE TO PROCESS THE UPLOADED FILE AND CALL cv_parser.py MODULE
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    path = UPLOAD_FOLDER + "/" + filename
+    fname_without_ext, file_extension = os.path.splitext(filename)
+    output_fname = "Data_" + fname_without_ext + ".xlsx"
+    df = extract_info(path)
+    writer = pd.ExcelWriter(
+        "./Output_Files/" + output_fname, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name="Sheet 1", index=False)
+    writer.save()
+    return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename=output_fname, as_attachment="True")
+
+# FUNCTION TO STORE THE FILE NAME AND FILE DATA (BLOB) INTO A SQLite3 DATABASE
+
+
+def database(name, data):
+    conn = sqlite3.connect("Resume_parser.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS resume_files (name TEXT,data BLOB) """)
+    cursor.execute(
+        """INSERT INTO resume_files (name, data) VALUES (?,?) """, (name, data))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ cv_parser.py
@@ -0,0 +1,227 @@
+import re
+import nltk
+from docx import Document
+import os
+import textract
+import pandas as pd
+from PyPDF2 import PdfFileReader
+import PyPDF2
+
+import time
+
+nltk.download('punkt', quiet=True)
+nltk.download('averaged_perceptron_tagger', quiet=True)
+######################################################################
+# USE THIS FUNCTION ONLY IF  file_To_Text() DOESNT WORK FOR PDF FILES
+
+
+def pdf_To_Text(path):
+    pdfFileObj = open(path, 'rb')
+    pdfReader = PdfFileReader(pdfFileObj)
+    pageObj = pdfReader.getPage(0)
+    txt = pageObj.extractText()
+    pdfFileObj.close()
+    return txt
+
+######################################################################
+
+
+def file_To_Text(path):
+    text = textract.process(path)
+    return text.decode("utf-8")
+
+#----------------------------------------------------------------------------------------------------
+# Function to extract the font names from PDF files
+
+
+def extract_font_pdf(fname):
+    def walk(obj, fnt, emb):
+        if not hasattr(obj, 'keys'):
+            return None, None
+        fontkeys = set(['/FontFile', '/FontFile2', '/FontFile3'])
+        if '/BaseFont' in obj:
+            fnt.add(obj['/BaseFont'])
+        if '/FontName' in obj:
+            if [x for x in fontkeys if x in obj]:  # test to see if there is FontFile
+                emb.add(obj['/FontName'])
+        for k in obj.keys():
+            walk(obj[k], fnt, emb)
+        return fnt, emb
+
+    pdf = PdfFileReader(fname)
+    fonts = set()
+    embedded = set()
+    for page in pdf.pages:
+        obj = page.getObject()
+        if type(obj) == PyPDF2.generic.ArrayObject:
+            for i in obj:
+                if hasattr(i, 'keys'):
+                    f, e = walk(i, fonts, embedded)
+                    fonts = fonts.union(f)
+                    embedded = embedded.union(e)
+        else:
+            f, e = walk(obj['/Resources'], fonts, embedded)
+            fonts = fonts.union(f)
+            embedded = embedded.union(e)
+
+    font = sorted(list(fonts))
+    for i in range(len(font)):
+        font[i] = font[i].rstrip("MT")
+        font[i] = font[i].lstrip("/")
+    return font
+
+#----------------------------------------------------------------------------------------------------
+# Function to extract the font name, font size, number of tables, images from a docx file
+
+
+def extract_font_table_imgs_docx(path):
+    doc = Document(path)
+    font = []
+    for p in doc.paragraphs:
+        for r in p.runs:
+            name = r.font.name
+            size = r.font.size
+            if size is not None:
+                size = size / 12700
+            if name is not None:
+                if (name, size) not in font:
+                    font.append((name, size))
+    table_count = len(doc.tables)
+    img_count = len(doc.inline_shapes)
+    return font, table_count, img_count
+
+#----------------------------------------------------------------------------------------------------
+# Function to extract email ids from both PDF and DOCX files
+
+
+def extract_emails(txt):
+    emails = re.findall(
+        "([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", txt)
+    if emails == []:
+        emails.append(None)
+    return emails
+#----------------------------------------------------------------------------------------------------
+# Function to extract mobile numbers from both PDF and DOCX files
+
+
+def extract_mobile_number(text):
+    phone = re.findall(re.compile(
+        r'(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?'), text)
+    if phone:
+        number = ''.join(phone[0])
+        if len(number) > 10:
+            return '+' + number
+        else:
+            return number
+
+#----------------------------------------------------------------------------------------------------
+# Function to extract the LinkedIn URL of user from both PDF and DOCX files
+
+
+def extract_Linkedin(txt):
+    url = re.search(
+        r"http(s)?:\/\/([\w]+\.)?linkedin\.com\/in\/[A-z0-9_-]+\/?", txt)
+    if url == None:
+        return None
+    else:
+        return url.group()
+
+#----------------------------------------------------------------------------------------------------
+# Function to extract the "INDIAN NAMES" from both PDF and DOCX files
+
+
+def extract_name(document):
+
+    # Reads Indian Names from the file, reduce all to lower case for easy comparision [Name lists]
+    indianNames = open(r"indian_names.txt", "r").read().lower()
+    indianNames = set(indianNames.split())
+    otherNameHits = []
+    nameHits = []
+    name = None
+
+    lines = [el.strip() for el in document.split("\n") if len(el)
+             > 0]  # Splitting on the basis of newlines
+    lines = [nltk.word_tokenize(el) for el in lines]
+    lines = [nltk.pos_tag(el) for el in lines]
+
+    sentences = nltk.sent_tokenize(document)
+    # Split/Tokenize sentences into words (List of lists of strings)
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    tokens = sentences
+    # Tag the tokens - list of lists of tuples - each tuple is (<word>, <tag>)
+    sentences = [nltk.pos_tag(sent) for sent in sentences]
+    dummy = []
+    for el in tokens:
+        dummy += el
+    tokens = dummy
+    # Try a regex chunk parser
+    grammar = r'NAME: {<NN.*><NN.*><NN.*>*}'
+    chunkParser = nltk.RegexpParser(grammar)
+    all_chunked_tokens = []
+    for tagged_tokens in lines:
+        # Creates a parse tree
+        if len(tagged_tokens) == 0:
+            continue
+        chunked_tokens = chunkParser.parse(tagged_tokens)
+        all_chunked_tokens.append(chunked_tokens)
+        for subtree in chunked_tokens.subtrees():
+            if subtree.label() == 'NAME':
+                for ind, leaf in enumerate(subtree.leaves()):
+                    if leaf[0].lower() in indianNames and 'NN' in leaf[1]:
+                        hit = " ".join([el[0]
+                                        for el in subtree.leaves()[ind:ind + 3]])
+                        if re.compile(r'[\d,:]').search(hit):
+                            continue
+                        nameHits.append(hit)
+    if len(nameHits) > 0:
+        nameHits = [re.sub(r'[^a-zA-Z \-]', '', el).strip() for el in nameHits]
+        name = " ".join([el[0].upper() + el[1:].lower()
+                         for el in nameHits[0].split() if len(el) > 0])
+        otherNameHits = nameHits[1:]
+    return name, otherNameHits
+
+#----------------------------------------------------------------------------------------------------
+# Master function which combines all above methods to return a data frame
+
+
+def extract_info(path):
+    file_name, file_extension = os.path.splitext(path)
+    if file_extension == ".pdf":
+        txt = file_To_Text(path)
+        font = extract_font_pdf(path)
+        table_count = "NA"
+        img_count = "NA"
+
+    elif file_extension == ".docx":
+        txt = file_To_Text(path)
+        font, table_count, img_count = extract_font_table_imgs_docx(path)
+    else:
+        return "Invalid Format"
+
+    linkedin = extract_Linkedin(txt)
+    mobile = extract_mobile_number(txt)
+    email = extract_emails(txt)
+    name = extract_name(txt)[0]
+    lines = len(txt.split("\n"))
+    words = len(txt.split())
+    chars = len(txt)
+
+    data = [{
+        "File Name": file_name + file_extension,
+        "Name": name,
+            "Contact Number": str(mobile),
+            "Email ID(s)": str(email),
+            "Linkedin URL": str(linkedin),
+            "Total Lines": lines,
+            "Total Characters": chars,
+            "Total Words": words,
+            "Fonts and Font sizes used": font,
+            "Total number of Tables": table_count,
+            "Total number of Images": img_count
+            }]
+    df = pd.DataFrame(data)
+    return df
+#----------------------------------------------------------------------------------------------------
+
+
+print("hello")
